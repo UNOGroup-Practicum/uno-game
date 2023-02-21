07e8c66682ed46c051f84bcca88b1db8e2e37e43
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2023-02-21 09:15:19 +0300

    modified:   packages/client/index.html 	modified:   packages/client/package.json 	renamed:    packages/client/src/App.css -> packages/client/src/components/app/App.css 	renamed:    packages/client/src/App.test.tsx -> packages/client/src/components/app/App.test.tsx 	renamed:    packages/client/src/App.tsx -> packages/client/src/components/app/App.tsx 	modified:   packages/client/src/index.css 	modified:   packages/client/src/main.tsx 	new file:   packages/client/src/redux/hooks.ts 	new file:   packages/client/src/redux/store.ts 	modified:   yarn.lock

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './components/app/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
