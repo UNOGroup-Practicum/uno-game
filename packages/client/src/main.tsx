36b1063936a5d24282aaa524b16c2b86c7c53c5f
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-02-21 22:47:20 +0300

    style: сбрасывает на дефолтные сомнительные настройки prettier

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,14 +1,14 @@
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/app/App'
-import './index.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
