3f03ba0c6a3fbf3e5bab854c7d553525d80265ed
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-02-24 18:53:37 +0300

    feat: реализована верстка шапки сайта

--- packages/client/src/theme/ThemeProvider.tsx
+++ packages/client/src/theme/ThemeProvider.tsx
@@ -1,10 +1,23 @@
 import React, { useMemo, useState } from "react";
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import { ThemeProvider as ThemeProviderMui, createTheme } from "@mui/material/styles";
+import { muiPallete } from "./muiPallete";
 
-const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.DARK;
 
 const ThemeProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const themeMui = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: theme,
+          ...muiPallete(theme),
+        },
+      }),
+    [theme]
+  );
 
   const defaultProps = useMemo(
     () => ({
@@ -14,7 +27,11 @@ const ThemeProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
     [theme]
   );
 
-  return <ThemeContext.Provider value={defaultProps}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeProviderMui theme={themeMui}>
+      <ThemeContext.Provider value={defaultProps}>{children}</ThemeContext.Provider>
+    </ThemeProviderMui>
+  );
 };
 
 export default ThemeProvider;
