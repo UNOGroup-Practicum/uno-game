45fd08b39f9158ce0540fc35e850e10248f9f137
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-02-26 08:50:37 +0300

    feat: реализована верстка подвала сайта

--- packages/client/src/components/app-footer/AppFooter.tsx
+++ packages/client/src/components/app-footer/AppFooter.tsx
@@ -1,11 +1,26 @@
-import clsx from "clsx";
-import { Container } from "@mui/material";
-import styles from "./AppFooter.module.scss";
+import { Container, Box, Typography, Link } from "@mui/material";
+import { routes } from "../../constants";
 
 export const AppFooter = () => {
   return (
-    <div className={clsx("app-footer", styles.root)}>
-      <Container maxWidth="md">footer</Container>
-    </div>
+    <Box
+      sx={{
+        marginTop: "auto",
+        padding: "1rem 0 1.2rem",
+        background: "var(--primary-light-color)",
+        boxShadow: "inset 0 1px 0 0 rgb(255 255 255 / 10%)",
+      }}
+    >
+      <Container maxWidth="md">
+        <Link href={routes.home.path}>
+          <Typography align="center" fontSize="20px" color="text.secondary" fontWeight="bold">
+            Uno
+          </Typography>
+        </Link>
+        <Typography align="center" fontSize="14px" color="text.disabled">
+          Учебный проект, разработанный в рамках курса "Мидл фронтенд-разработчик".
+        </Typography>
+      </Container>
+    </Box>
   );
 };
