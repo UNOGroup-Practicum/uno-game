3eefe744dfaa7cde7b082018d269b7b63d71c972
AleksandrMenshchikov <51015585+AleksandrMenshchikov@users.noreply.github.com>
2023-03-07 21:38:46 +0300

    feat: create page /game-preparing (#23)

--- packages/client/src/services/reducers.ts
+++ packages/client/src/services/reducers.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { authSlice } from "./slices/auth-slice";
+import { gameSlice } from "./slices/gameSlice";
 
 export const rootReducer = combineReducers({
   auth: authSlice.reducer,
+  game: gameSlice.reducer,
 });
---
+++ packages/client/src/services/slices/gameSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+type TState = {
+  gameVariant: "solo" | "withFriends" | null;
+};
+
+const initialState: TState = {
+  gameVariant: null,
+};
+
+export const gameSlice = createSlice({
+  name: "game",
+  initialState,
+  reducers: {
+    setGameVariant: (state, action: PayloadAction<TState[keyof Pick<TState, "gameVariant">]>) => {
+      state.gameVariant = action.payload;
+    },
+  },
+});
+
+export const gameSelect = (state: RootState) => state.game;
