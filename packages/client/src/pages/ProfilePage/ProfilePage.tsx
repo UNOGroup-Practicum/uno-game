7c5e17ec76b34653733d31e93696fac107c22398
Roman Makhov <nekromantos30888666@gmail.com>
2023-03-21 17:35:55 +0300

    feat: добавляет раздел со сменой пароля

--- packages/client/src/pages/ProfilePage/ProfilePage.tsx
+++ packages/client/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,15 +1,20 @@
 import EditIcon from "@mui/icons-material/Edit";
 import { Avatar, Box, Button, Container, Link, Stack, TextField, Typography } from "@mui/material";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 
-import { UpdateUserRequestData } from "services/api/types";
+import { UpdateUserPassword, UpdateUserRequestData } from "services/api/types";
 import { useDispatch } from "services/hooks";
 import { authSelect } from "services/slices/auth-slice";
 import { userSelect, userSlice, userThunks } from "services/slices/user-slice";
-import { InputNames, validationTemplate } from "utils/validation/validation";
+import {
+  ERROR_MESSAGE,
+  InputNames,
+  REQUIRED_MESSAGE,
+  validationTemplate,
+} from "utils/validation/validation";
 
 import { ROUTES } from "../../constants";
 
@@ -22,6 +27,8 @@ export const ProfilePage: React.FC = () => {
 
   const [isInputChanged, setIsInputChanged] = useState(false);
 
+  const [isChangePassword, setIsChangePassword] = useState(false);
+
   const { user } = useSelector(authSelect);
 
   const transformedUser: UpdateUserRequestData = {
@@ -33,6 +40,12 @@ export const ProfilePage: React.FC = () => {
     email: user?.email || "",
   };
 
+  const updatedUserPassword = {
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  };
+
   const {
     control,
     handleSubmit,
@@ -41,9 +54,12 @@ export const ProfilePage: React.FC = () => {
   } = useForm({
     defaultValues: {
       ...transformedUser,
+      ...updatedUserPassword,
     },
     mode: "onChange",
   });
+
+  const passwordNewRef = useRef<HTMLInputElement>();
 
   useEffect(() => {
     reset(transformedUser);
@@ -81,6 +97,25 @@ export const ProfilePage: React.FC = () => {
   const onChangeProfile: SubmitHandler<UpdateUserRequestData> = (formData) => {
     dispatch(userThunks.changeUserProfile(formData));
   };
+
+  const onOpenFormChangePassword = () => {
+    setIsChangePassword(!isChangePassword);
+  };
+
+  const onSubmitChangePassword: SubmitHandler<UpdateUserPassword> = (formData) => {
+    const { oldPassword, newPassword } = formData;
+    console.log(
+      JSON.stringify(
+        {
+          oldPassword: oldPassword,
+          newPassword: newPassword,
+        },
+        null,
+        2
+      )
+    );
+  };
+
   return (
     <main className={styles.profile} data-testid="page-profile">
       <Container maxWidth="md">
@@ -108,161 +143,256 @@ export const ProfilePage: React.FC = () => {
             </Box>
           )}
         </section>
-
-        <form
-          name="profile-form"
-          className={styles.profile__form}
-          onSubmit={handleSubmit(onChangeProfile)}
-          noValidate
-        >
-          <Stack spacing={3}>
-            <Controller
-              name="email"
-              rules={validationTemplate(InputNames.EMAIL)}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Почта"
-                  type="email"
-                  id="email"
-                  variant="standard"
-                  InputLabelProps={{ shrink: false }}
-                  onChange={field.onChange}
-                  onInput={() => setIsInputChanged(true)}
-                  value={field.value}
-                  error={!!errors.email?.message}
-                  helperText={errors.email?.message}
-                />
-              )}
-            />
-            <Controller
-              name="login"
-              rules={validationTemplate(InputNames.LOGIN)}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Логин"
-                  id="login"
-                  variant="standard"
-                  InputLabelProps={{ shrink: false }}
-                  onChange={field.onChange}
-                  onInput={() => setIsInputChanged(true)}
-                  value={field.value}
-                  error={!!errors.login?.message}
-                  helperText={errors.login?.message}
-                />
-              )}
-            />
-            <Controller
-              name="display_name"
-              rules={validationTemplate(InputNames.NAME)}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Отображаемое имя"
-                  id="display_name"
-                  variant="standard"
-                  InputLabelProps={{ shrink: false }}
-                  onChange={field.onChange}
-                  onInput={() => setIsInputChanged(true)}
-                  value={field.value}
-                  error={!!errors.display_name?.message}
-                  helperText={errors.display_name?.message}
-                />
-              )}
-            />
-            <Controller
-              name="first_name"
-              rules={validationTemplate(InputNames.NAME)}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Имя"
-                  id="first_name"
-                  variant="standard"
-                  InputLabelProps={{ shrink: false }}
-                  onChange={field.onChange}
-                  onInput={() => setIsInputChanged(true)}
-                  value={field.value}
-                  error={!!errors.first_name?.message}
-                  helperText={errors.first_name?.message}
-                />
-              )}
-            />
-            <Controller
-              name="second_name"
-              rules={validationTemplate(InputNames.NAME)}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  variant="standard"
-                  label="Фамилия"
-                  id="second_name"
-                  InputLabelProps={{ shrink: false }}
-                  onChange={field.onChange}
-                  onInput={() => setIsInputChanged(true)}
-                  value={field.value}
-                  error={!!errors.second_name?.message}
-                  helperText={errors.second_name?.message}
-                />
-              )}
-            />
-            <Controller
-              name="phone"
-              rules={validationTemplate(InputNames.PHONE)}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  type="tel"
-                  label="Телефон"
-                  id="phone"
-                  variant="standard"
-                  InputLabelProps={{ shrink: false }}
-                  onChange={field.onChange}
-                  onInput={() => setIsInputChanged(true)}
-                  value={field.value}
-                  error={!!errors.phone?.message}
-                  helperText={errors.phone?.message}
-                />
-              )}
-            />
-          </Stack>
-
-          {isLoading.profile && (
-            <Box mt={2} textAlign={"center"}>
-              загрузка...
-            </Box>
-          )}
-          {isSuccess.profile && (
-            <Box mt={2} textAlign={"center"}>
-              Данные успешно обновлены!
-            </Box>
-          )}
-          {error.profile && (
-            <Box mt={2} textAlign={"center"} color={"error.light"}>
-              {error.profile}
-            </Box>
-          )}
-
-          <Button
-            fullWidth={true}
-            size="large"
-            type="submit"
-            variant="contained"
-            color="warning"
-            sx={{
-              marginTop: "50px",
-              marginBottom: "10px",
-            }}
-            disabled={!(isValid && isInputChanged)}
+        {!isChangePassword ? (
+          <form
+            name="profile-form"
+            className={styles.profile__form}
+            onSubmit={handleSubmit(onChangeProfile)}
+            noValidate
           >
-            Сохранить
-          </Button>
-        </form>
+            <Stack spacing={3}>
+              <Controller
+                name="email"
+                rules={validationTemplate(InputNames.EMAIL)}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    label="Почта"
+                    type="email"
+                    id="email"
+                    variant="standard"
+                    InputLabelProps={{ shrink: false }}
+                    onChange={field.onChange}
+                    onInput={() => setIsInputChanged(true)}
+                    value={field.value}
+                    error={!!errors.email?.message}
+                    helperText={errors.email?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="login"
+                rules={validationTemplate(InputNames.LOGIN)}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    label="Логин"
+                    id="login"
+                    variant="standard"
+                    InputLabelProps={{ shrink: false }}
+                    onChange={field.onChange}
+                    onInput={() => setIsInputChanged(true)}
+                    value={field.value}
+                    error={!!errors.login?.message}
+                    helperText={errors.login?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="display_name"
+                rules={validationTemplate(InputNames.NAME)}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    label="Отображаемое имя"
+                    id="display_name"
+                    variant="standard"
+                    InputLabelProps={{ shrink: false }}
+                    onChange={field.onChange}
+                    onInput={() => setIsInputChanged(true)}
+                    value={field.value}
+                    error={!!errors.display_name?.message}
+                    helperText={errors.display_name?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="first_name"
+                rules={validationTemplate(InputNames.NAME)}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    label="Имя"
+                    id="first_name"
+                    variant="standard"
+                    InputLabelProps={{ shrink: false }}
+                    onChange={field.onChange}
+                    onInput={() => setIsInputChanged(true)}
+                    value={field.value}
+                    error={!!errors.first_name?.message}
+                    helperText={errors.first_name?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="second_name"
+                rules={validationTemplate(InputNames.NAME)}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    variant="standard"
+                    label="Фамилия"
+                    id="second_name"
+                    InputLabelProps={{ shrink: false }}
+                    onChange={field.onChange}
+                    onInput={() => setIsInputChanged(true)}
+                    value={field.value}
+                    error={!!errors.second_name?.message}
+                    helperText={errors.second_name?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="phone"
+                rules={validationTemplate(InputNames.PHONE)}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    type="tel"
+                    label="Телефон"
+                    id="phone"
+                    variant="standard"
+                    InputLabelProps={{ shrink: false }}
+                    onChange={field.onChange}
+                    onInput={() => setIsInputChanged(true)}
+                    value={field.value}
+                    error={!!errors.phone?.message}
+                    helperText={errors.phone?.message}
+                  />
+                )}
+              />
+            </Stack>
 
+            {isLoading.profile && (
+              <Box mt={2} textAlign={"center"}>
+                загрузка...
+              </Box>
+            )}
+            {isSuccess.profile && (
+              <Box mt={2} textAlign={"center"}>
+                Данные успешно обновлены!
+              </Box>
+            )}
+            {error.profile && (
+              <Box mt={2} textAlign={"center"} color={"error.light"}>
+                {error.profile}
+              </Box>
+            )}
+
+            <Button
+              fullWidth={true}
+              size="large"
+              type="submit"
+              variant="contained"
+              color="warning"
+              sx={{
+                marginTop: "50px",
+                marginBottom: "10px",
+              }}
+              disabled={!(isValid && isInputChanged)}
+            >
+              Сохранить
+            </Button>
+          </form>
+        ) : (
+          <form
+            name="changePassword-form"
+            key="changePassword-form"
+            className={styles.profile__form}
+            onSubmit={handleSubmit(onSubmitChangePassword)}
+            noValidate
+          >
+            <Stack spacing={3}>
+              <Controller
+                control={control}
+                key="oldPassword"
+                name="oldPassword"
+                rules={{ required: REQUIRED_MESSAGE }}
+                render={({ field }) => (
+                  <TextField
+                    variant="standard"
+                    label="Старый пароль"
+                    type="password"
+                    id="oldPassword"
+                    onChange={field.onChange}
+                    value={field.value || ""}
+                    error={!!errors.oldPassword?.message}
+                    helperText={errors.oldPassword?.message}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="newPassword"
+                key="newPassword"
+                rules={validationTemplate(InputNames.PASSWORD)}
+                render={({ field }) => (
+                  <TextField
+                    variant="standard"
+                    label="Новый пароль"
+                    type="password"
+                    id="newPassword"
+                    inputRef={passwordNewRef}
+                    onChange={field.onChange}
+                    value={field.value || ""}
+                    error={!!errors.newPassword?.message}
+                    helperText={errors.newPassword?.message}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="confirmPassword"
+                key="confirmPassword"
+                rules={{
+                  required: REQUIRED_MESSAGE,
+                  validate: (value: string) => {
+                    if (value !== passwordNewRef.current?.value) {
+                      return ERROR_MESSAGE.CONFIRM_PASSWORD;
+                    }
+
+                    return true;
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    variant="standard"
+                    label="Подтвердите новый пароль"
+                    type="password"
+                    id="confirmPassword"
+                    onChange={field.onChange}
+                    value={field.value || ""}
+                    error={!!errors.confirmPassword?.message}
+                    helperText={errors.confirmPassword?.message}
+                  />
+                )}
+              />
+            </Stack>
+            <Button
+              fullWidth={true}
+              size="large"
+              type="submit"
+              variant="contained"
+              color="warning"
+              sx={{
+                marginTop: "50px",
+                marginBottom: "10px",
+              }}
+              disabled={!isValid}
+            >
+              Сохранить
+            </Button>
+          </form>
+        )}
         <Link href={ROUTES.profile.path} underline="none">
-          <Typography align="center" fontSize="16px" color="text.disabled" fontWeight="bold">
-            Сменить пароль
+          <Typography
+            align="center"
+            fontSize="16px"
+            color="text.disabled"
+            fontWeight="bold"
+            onClick={onOpenFormChangePassword}
+          >
+            {isChangePassword ? "Редактировать профиль" : "Сменить пароль"}
           </Typography>
         </Link>
       </Container>
