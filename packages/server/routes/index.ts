b1cf3c92233dc0c23472514c661c89ccea7fef78
Evgeniy Abashin <aevgen.001@yandex.ru>
2023-05-01 13:47:14 +0300

    feat: добавляет проверку авторизации пользователя при запросах к /forum

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -1,40 +1,27 @@
-import { Router } from "express";
-import { YandexAPIService, ApiError } from "../api/YandexAPIService";
+import { NextFunction, Request, Response, Router } from "express";
 import theme from "./theme";
 import forum from "./forum";
+import { ApiError, YandexAPIService } from "../api/YandexAPIService";
+
+export const checkIsUserAuthorized = async (req: Request, res: Response, next: NextFunction) => {
+  const apiService = new YandexAPIService(req.headers["cookie"]);
+  try {
+    await apiService.getCurrentUser();
+    next();
+  } catch (error) {
+    if (error instanceof ApiError) {
+      res.status(error.status).set({ "Content-Type": "text/html" }).end(error.message);
+      return;
+    }
+    res.status(500).end();
+    return;
+  }
+};
 
 const router = Router();
 
 router.use("/theme", theme);
-router.use("/forum", forum);
-
-/**
- * TODO: Удалить, роут когда будет не нужен
- * Пример роута с аутентификацией пользователя
- * Ручка для запроса с клиента - http://localhost:3001/api/forum
- */
-router.use("/api/forum", async (req, res) => {
-  const apiService = new YandexAPIService(req.headers["cookie"]);
-  let user;
-
-  try {
-    user = await apiService.getCurrentUser();
-  } catch (error) {
-    if (error instanceof ApiError) {
-      res.status(error.status).set({ "Content-Type": "text/html" }).end(error.message);
-
-      return;
-    }
-
-    res.status(500).end();
-
-    return;
-  }
-
-  console.log("HERE: ", user);
-
-  res.status(200).set({ "Content-Type": "text/html" }).end("OK");
-});
+router.use("/forum", (req, res, next) => checkIsUserAuthorized(req, res, next), forum);
 
 router.use("/*", (req, res) =>
   res.status(400).json({ requestMethod: req.method, message: `Роут не найден` })
